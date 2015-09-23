6634da7cc804bf16e217c908f27bb382165706f7
David <Ecutwxguan@163.com>
2015-09-23 19:37:21 +0800

    Test still can not run, need fix.

---
+++ test/_test.js
@@ -0,0 +1,5 @@
+// test case coding without ES 6 syntax.
+var React = require('react/addons');
+var assert = require('assert');
+var TestUtils = React.addons.TestUtils;
+var RegionBar = require('../scripts/components/RegionBar')
