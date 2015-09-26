cecb2d964b2fd313eabdba0648faa9a83af865da
David <Ecutwxguan@163.com>
2015-09-26 22:35:08 +0800

    Add karma.

--- test/_test.js
+++ test/_test.js
@@ -1,9 +1,17 @@
-import React from 'react/addons'
-import { expect } from 'chai'
+import React from 'react/addons';
+import Header from '../scripts/components/Header/Header';
+let TestUtils = React.addons.TestUtils;
+
+describe('test case should faild', ()=> {
+  it('hello test case', function() {
+    expect(true).toBe(false);
+  });
+});
+
 
 describe('test case should work', ()=> {
-  it('hello test case', function(done) {
-    expect(2).to.equal(2);
-    done();
+  it('hello test case', function() {
+    let dom = TestUtils.renderIntoDocument(<Header />);
+    expect(dom !== undefined).toBe(true);
   });
 });
