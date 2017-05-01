b8f5ad9e0b5198eeea1512941087d099f51f40f3
Zhuo Zhang <contrary.zhuo@gmail.com>
2017-05-01 13:22:09 +0800

    Use CSS Modules

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -142,7 +142,7 @@ module.exports = {
         test: /\.css$/,
         loader: ExtractTextPlugin.extract(
           'style',
-          'css?importLoaders=1!postcss',
+          'css?importLoaders=1&modules&localIdentName=[name]__[local]___[hash:base64:5]!postcss',
           extractTextPluginOptions
         )
         // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
