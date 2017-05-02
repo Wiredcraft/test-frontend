ca2e5d2ee03e3145ed392a468e561b993aa2807d
Zhuo Zhang <contrary.zhuo@gmail.com>
2017-05-02 21:23:53 +0800

    Init store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
+import Store from './store'
+
 import './styles/global.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = new Store()
+store.createRandomRegions()
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'))
