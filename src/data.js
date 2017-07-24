1c3e6327979ed2f9e4d4c7fd74497f4f1214ab63
李磊 <lilei@lileideMacBook-Pro.local>
2017-07-25 00:51:04 +0800

    test-pr

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    level: 'state',
+    name: 'Shan state',
+    sub: [
+      {
+        level: 'district',
+        name: 'Aunglan',
+        sub: [
+          {
+            level: 'township',
+            name: 'Loilen'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    level: 'state',
+    name: 'ShangHai',
+    sub: [
+      {
+        level: 'district',
+        name: 'PuDong'
+      },
+      {
+        level: 'district',
+        name: 'JingAn'
+      }
+    ]
+  }
+]
---
+++ src/panel.js
@@ -0,0 +1,58 @@
+import React, {
+  Component
+} from 'react';
+import Item from './Item'
+import rawData from './data'
+
+
+let arr = [];
+function getData(data, level, keyWord) {
+  data.forEach((item) => {
+    if (item.level === level) {
+      if (keyWord === '' || item.name.indexOf(keyWord) !== -1) {
+        arr.push(item)
+      }
+    } else if (item.sub && item.sub.length > 0) {
+      getData(item.sub, level, keyWord);
+    }
+  })
+  return arr;
+}
+
+
+class panel extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: []
+    }
+  }
+
+  renderLocation() {
+    arr = [];
+    let data = getData(rawData, this.props.startLevel, this.props.keyWord);
+    return data.map((item, i) => {
+      return (
+        <Item
+          name={item.name}
+          startLevel={this.props.startLevel}
+          level={item.level}
+          filter={this.props.filter}
+          keyWord={this.keyWord}
+          sub={item.sub ? item.sub : []}
+          key={i + item.name}
+        />
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div > {this.renderLocation()} </div>
+    )
+  }
+}
+
+
+export default panel
