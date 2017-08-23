27e2b66df22ec7e226cc3ad9e1e70c68619296cd
Francisco <fbricenho@gmail.com>
2017-08-23 10:59:54 +0200

    Dropdown filter implementation and styling

--- src/main/searchBar/searchBar.js
+++ src/main/searchBar/searchBar.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import './searchBar.css'
+import Dropdown from 'react-dropdown'
+const options = [
+  'Filter', 'State', 'District', 'Township'
+]
+const defaultOption = options[0]
 
 class SearchBar extends Component {
   render () {
     return (
       <div>
-        <h3>Search Bar</h3>
+        <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder='Select an option' className='Dropdown' />
       </div>
     )
   }
