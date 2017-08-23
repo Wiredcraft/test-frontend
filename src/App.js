f1b715d0953e6bf9b8119ea228fa01d1e3addfd9
Francisco <fbricenho@gmail.com>
2017-08-23 08:54:22 +0200

    create component main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import Header from './header/header'
+import Main from './main/main'
 import './App.css'
 
 class App extends Component {
   render () {
     return (
-      <Header />
+      <div>
+        <Header />
+        <Main />
+      </div>
     )
   }
 }
---
+++ src/main/main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './main.css'
+
+class Main extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Hello world</h1>
+      </div>
+    )
+  }
+}
+
+export default Main
