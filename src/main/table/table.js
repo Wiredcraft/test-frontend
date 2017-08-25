c5bfd308767791bbb9003fd99e4409b30da2e283
Francisco <fbricenho@gmail.com>
2017-08-25 14:47:38 +0200

    label for toggle

--- src/main/table/table.js
+++ src/main/table/table.js
@@ -6,14 +6,20 @@ import download from '../../assets/download.png'
 let data = [
   {
     name: 'Shan state',
+    type: 'State',
     district: [
       {
         name: 'Aunglan',
+        type: 'District',
         township: [
           {
             name: 'Loilen'
           }
-        ]
+        ],
+        last: 123456,
+        forms: 342456,
+        voters: 123456,
+        update: 342456
       }
     ],
     last: 123456,
@@ -23,14 +29,24 @@ let data = [
   },
   {
     name: 'Shan state',
+    type: 'State',
     district: [
       {
         name: 'Aunglan',
+        type: 'District',
         township: [
           {
-            name: 'Loilen'
+            name: 'Loilen',
+            last: 123456,
+            forms: 342456,
+            voters: 123456,
+            update: 342456
           }
-        ]
+        ],
+        last: 123456,
+        forms: 342456,
+        voters: 123456,
+        update: 342456
       }
     ],
     last: 123456,
@@ -40,6 +56,26 @@ let data = [
   },
   {
     name: 'Shan state',
+    type: 'State',
+    district: [
+      {
+        name: 'Aunglan',
+        type: 'District',
+        township: [
+          {
+            name: 'Loilen',
+            last: 123456,
+            forms: 342456,
+            voters: 123456,
+            update: 342456
+          }
+        ],
+        last: 123456,
+        forms: 342456,
+        voters: 123456,
+        update: 342456
+      }
+    ],
     last: 123456,
     forms: 342456,
     voters: 123456,
@@ -47,9 +83,24 @@ let data = [
   },
   {
     name: 'Shan state',
+    type: 'State',
     district: [
       {
-        name: 'Aunglan'
+        name: 'Aunglan',
+        type: 'District',
+        township: [
+          {
+            name: 'Loilen',
+            last: 123456,
+            forms: 342456,
+            voters: 123456,
+            update: 342456
+          }
+        ],
+        last: 123456,
+        forms: 342456,
+        voters: 123456,
+        update: 342456
       }
     ],
     last: 123456,
@@ -63,25 +114,37 @@ class Table extends Component {
   constructor () {
     super()
     this.handleClick = this.handleClick.bind(this)
+    this.printTable = this.printTable.bind(this)
   }
   handleClick (i) {
     console.log(i)
+  }
+  printTable (e, i) {
+    return <a className='row' id={'id:' + i}key={i} onClick={() => this.handleClick(i)}>
+      <div className='item big'>
+        <Avatar name={e.name} size={20} textSizeRatio={1.6} round />
+        <p className={e.type.toLowerCase()}>
+          {e.name}
+        </p>
+        <img className='download' src={download} alt='download' />
+        <div className='nameLabel'>
+          <bold> {e.district.length}</bold>
+          {e.type}
+          <span className='more'>+</span>
+          <span className='less'>-</span>
+        </div>
+      </div>
+      <div className='item small'>{e.last.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}</div>
+      <div className='item small'>{e.forms.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}</div>
+      <div className='item small'>{e.voters.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}</div>
+      <div className='item small'>{e.update.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}</div>
+    </a>
   }
   render () {
     return (
       <div>
         {data.map((e, i) => {
-          return <a href='' className='row' id={'id:' + i}key={i} onClick={() => this.handleClick(i)}>
-            <div className='item big'>
-              <Avatar name={e.name} size={20} round />
-              {e.name}
-              <img className='download' src={download} />
-            </div>
-            <div className='item small'>{e.last.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}</div>
-            <div className='item small'>{e.forms.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}</div>
-            <div className='item small'>{e.voters.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}</div>
-            <div className='item small'>{e.update.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}</div>
-          </a>
+          return this.printTable(e, i)
         }) }
       </div>
     )
