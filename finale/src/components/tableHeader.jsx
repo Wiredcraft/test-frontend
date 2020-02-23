9e61578d1bb8ddead65f34bb65c6d2023b6a0033
Yi Zu <zuyi@bu.edu>
2020-02-23 17:52:44 +0800

    Completed table header

---
+++ finale/src/components/tableHeader.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class TableHeader extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+      //Data for header of table
+      names:
+        { col1: "Region", col2: "Last Input", col3: "Number of Forms", col4: "Number of Voters", col5: "Update" }
+    }
+  }
+
+  //Get names of columns and render table header
+  renderTableHeader() {
+    let header = Object.values(this.state.names);
+    console.log(header);
+    return header.map((key, index) => {
+      return <th key={index}>{key}</th>
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <table class="names">
+          <tbody>
+            <tr>{this.renderTableHeader()}</tr>
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
+export default TableHeader;
