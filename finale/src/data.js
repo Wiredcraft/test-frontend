20eab33fa0f9f6f6c5f65b981cf0c6fcaee14d10
Yi Zu <zuyi@bu.edu>
2020-02-29 12:58:56 +0800

    Added mock data for table

---
+++ finale/src/data.js
@@ -0,0 +1,194 @@
+export default [
+  {
+    id: "0001",
+    title: "Shanghai State",
+    level: "State",
+    lastIn: "2020/02/02",
+    numForms: "123,456",
+    numVotes: "123,456",
+    update: "342,456",
+    isHidden: false,
+    subRegions: [
+      {
+        id: "00011",
+        title: "Jingan District",
+        level: "District",
+        lastIn: "2020/02/02",
+        numForms: "123,456",
+        numVotes: "123,456",
+        update: "342,456",
+        isHidden: true,
+        subRegions: []
+      },
+      {
+        id: "00012",
+        title: "Pudong District",
+        level: "District",
+        lastIn: "2020/02/02",
+        numForms: "123,456",
+        numVotes: "123,456",
+        update: "342,456",
+        subRegions: [
+          {
+            id: "000121",
+            title: "Lujiazui Township",
+            level: "Township",
+            lastIn: "2020/02/02",
+            numForms: "123,456",
+            numVotes: "123,456",
+            update: "342,456",
+            isHidden: true,
+            subRegions: []
+          },
+          {
+            id: "000122",
+            title: "Zhangjiang Township",
+            level: "Township",
+            lastIn: "2020/02/02",
+            numForms: "123,456",
+            numVotes: "123,456",
+            update: "342,456",
+            isHidden: true,
+            subRegions: []
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: "0002",
+    title: "Beijing State",
+    level: "State",
+    lastIn: "2020/02/02",
+    numForms: "123,456",
+    numVotes: "123,456",
+    update: "342,456",
+    isHidden: false,
+    subRegions: [
+      {
+        id: "00021",
+        title: "xichen District",
+        level: "District",
+        lastIn: "2020/02/02",
+        numForms: "123,456",
+        numVotes: "123,456",
+        update: "342,456",
+        isHidden: true,
+        subRegions: []
+      },
+      {
+        id: "00022",
+        title: "dongcheng District",
+        level: "District",
+        lastIn: "2020/02/02",
+        numForms: "123,456",
+        numVotes: "123,456",
+        update: "342,456",
+        isHidden: true,
+        subRegions: [
+          {
+            id: "000221",
+            title: "congwen Township",
+            level: "Township",
+            lastIn: "2020/02/02",
+            numForms: "123,456",
+            numVotes: "123,456",
+            update: "342,456",
+            isHidden: true,
+            subRegions: []
+          },
+          {
+            id: "000222",
+            title: "jianguo Township",
+            level: "Township",
+            lastIn: "2020/02/02",
+            numForms: "123,456",
+            numVotes: "123,456",
+            update: "342,456",
+            isHidden: true,
+            subRegions: []
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: "0003",
+    title: "Wuhan State",
+    level: "State",
+    lastIn: "2020/02/02",
+    numForms: "123,456",
+    numVotes: "123,456",
+    update: "342,456",
+    isHidden: false,
+    subRegions: [
+      {
+        id: "00031",
+        title: "Qingshan District",
+        level: "District",
+        lastIn: "2020/02/02",
+        numForms: "123,456",
+        numVotes: "123,456",
+        update: "342,456",
+        isHidden: true,
+        subRegions: []
+      },
+      {
+        id: "00032",
+        title: "Hankou District",
+        level: "District",
+        lastIn: "2020/02/02",
+        numForms: "123,456",
+        numVotes: "123,456",
+        update: "342,456",
+        isHidden: true,
+        subRegions: [
+          {
+            id: "000321",
+            title: "hanyang Township",
+            level: "Township",
+            lastIn: "2020/02/02",
+            numForms: "123,456",
+            numVotes: "123,456",
+            update: "342,456",
+            isHidden: true,
+            subRegions: []
+          },
+          {
+            id: "000322",
+            title: "wudaokou Township",
+            level: "Township",
+            lastIn: "2020/02/02",
+            numForms: "123,456",
+            numVotes: "123,456",
+            update: "342,456",
+            isHidden: true,
+            subRegions: []
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: "0004",
+    title: "Shan State",
+    level: "State",
+    lastIn: "2020/02/02",
+    numForms: "123,456",
+    numVotes: "123,456",
+    update: "342,456",
+    isHidden: false,
+    subRegions: []
+  },
+  {
+    id: "0005",
+    title: "Shan State",
+    level: "State",
+    lastIn: "2020/02/02",
+    numForms: "123,456",
+    numVotes: "123,456",
+    update: "342,456",
+    isHidden: false,
+    subRegions: []
+  }
+];
