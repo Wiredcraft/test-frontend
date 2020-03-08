4f624d0389c14b3e14ca9c0ce37726addb59ac7a
Yi Zu <zuyi@bu.edu>
2020-03-08 23:59:10 +0800

    Latest district component

--- finale/src/components/district.jsx
+++ finale/src/components/district.jsx
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import data from "../data"
 import Township from './township';
-import Township2 from './township2';
-import Township3 from './township3';
 import Township4 from './township4';
+import Township5 from './township5';
 
 class District extends Component {
   constructor() {
@@ -26,26 +25,28 @@ class District extends Component {
   }
 
   //Rendering function for district row
-  renderItem(item) {
-    const clickCallback = () => this.handleRowClick(item.id);
+  renderItem(district) {
+    const clickCallback = () => this.handleRowClick(district.id);
 
     const itemRows = [
-      <tr className="names" key={"row-data-" + item.id}>
-        <td> {item.title}
-          <button className="toggle-btn" onClick={clickCallback}>{item.subRegions.length} Townships</button>
+      <tr className="names" key={"row-data-" + district.id}>
+        <td> {district.title}
+
+          <button className="toggle-btn" onClick={clickCallback}>{district.subRegions.length} Townships</button>
+
         </td>
-        <td>{item.lastIn}</td>
-        <td>{item.numForms}</td>
-        <td>{item.numVotes}</td>
-        <td>{item.update}</td>
+        <td>{district.lastIn}</td>
+        <td>{district.numForms}</td>
+        <td>{district.numVotes}</td>
+        <td>{district.update}</td>
       </tr>
     ];
 
     //Toggle for rendering township rows of the district if there are townships under the district
-    if (item.subRegions.length > 0 && this.state.expandedRows.includes(item.id)) {
-      for (let i = 0; i < item.subRegions.length; i++) {
+    if (district.subRegions.length > 0 && this.state.expandedRows.includes(district.id)) {
+      for (let i = 0; i < district.subRegions.length; i++) {
         itemRows.push(
-          <Township />
+          <Township4 />
         )
       }
     }
@@ -56,27 +57,25 @@ class District extends Component {
     let allItemRows = [];
     let numStates = data.length;
 
-    // for(let i = 0; i < allItemRows.length; i++){
-    //   let perItemRows = this.renderItem(data[i].subRegions)
-    // }
-
-    for (let i = 0; i < numStates; i++) {
-      let numDist = data[i].subRegions.length;
+    for (let stateInd = 0; stateInd < numStates; stateInd++) {
+      let numDist = data[stateInd].subRegions.length;
+      console.log(stateInd)
 
       for (let dist = 0; dist < numDist; dist++) {
-        let perItemRows = this.renderItem(data[i].subRegions[dist]);
+
+        console.log(dist)
+
+        let perItemRows = this.renderItem(data[stateInd].subRegions[dist]);
 
         allItemRows = allItemRows.concat(perItemRows);
 
-        //Once you fix bug on township row to get town objects from right subRegion - put this line of code back 
-        // allItemRows = perItemRows;
-      }
-    }
+        // console.log(perItemRows);
 
-    // data.forEach(item => {
-    //   const perItemRows = this.renderItem(item);
-    //   allItemRows = allItemRows.concat(perItemRows);
-    // });
+        // allItemRows = allItemRows.concat(perItemRows);
+        return allItemRows;
+      }
+
+    }
 
     return (
       <tr>{allItemRows}</tr>
