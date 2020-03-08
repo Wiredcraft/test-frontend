761f03e4e40e1cba37195a532bc80b2749a8f3b8
Yi Zu <zuyi@bu.edu>
2020-03-09 00:00:07 +0800

    Latest App.js

--- finale/src/App.js
+++ finale/src/App.js
@@ -1,25 +1,23 @@
+//Use township4 and district component
+
 import React from "react";
 import "./App.css";
 import Navbar from "./components/navbar";
 import TableHeader from "./components/tableHeader";
 import State from "./components/state";
 import District from "./components/district";
+import District2 from "./components/district2";
 import Township from "./components/township";
-import Township2 from "./components/township2";
-import Township3 from "./components/township3";
 import Township4 from "./components/township4";
+import Township5 from "./components/township5";
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
       <TableHeader />
-      {/* <State /> */}
-      <District />
-      {/* <Township /> */}
-      {/* <Township2 />
-      <Township3 /> */}
-      {/* <Township4 /> */}
+      <State />
+      {/* <District /> */}
     </React.Fragment>
   );
 }
