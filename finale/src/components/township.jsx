b1713bdc9792e203ef7c176c92a574b4f5a4dcd0
Yi Zu <zuyi@bu.edu>
2020-04-19 19:00:49 +0800

    Changed township to stateless functional component as it does not have a state

--- finale/src/components/township.jsx
+++ finale/src/components/township.jsx
@@ -1,30 +1,22 @@
 import React, { Component } from 'react';
 import dl_logo from "../img/download_icon.png"
 
-class Township extends Component {
-  constructor() {
-    super();
-    this.state = {
-    }
-  }
 
-  render() {
-    const { title, id, lastIn, numForms, numVotes, update } = this.props.town;
-
-    return (
-      <tr className="townshipRow" key={"town-row-data-" + id}>
-        <td>
-          <span className="capital capitalTown">T</span>
-          <span>{title}</span>
-          <img className="dl_logo" src={dl_logo} alt="dl_icon" />
-        </td>
-        <td>{lastIn}</td>
-        <td>{numForms}</td>
-        <td>{numVotes}</td>
-        <td>{update}</td>
-      </tr>
-    )
-  }
+const Township = (props) => {
+  const { title, id, lastIn, numForms, numVotes, update } = props.town;
+  return (
+    <tr className="townshipRow" key={"town-row-data-" + id}>
+      <td>
+        <span className="capital capitalTown">T</span>
+        <span>{title}</span>
+        <img className="dl_logo" src={dl_logo} alt="dl_icon" />
+      </td>
+      <td>{lastIn}</td>
+      <td>{numForms}</td>
+      <td>{numVotes}</td>
+      <td>{update}</td>
+    </tr>
+  );
 }
 
 export default Township;
