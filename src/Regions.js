80eff3360f9388e4ba2b0a14a1f9184f406fa850
bundesl1ga <cameron.d.moreno@gmail.com>
2020-06-30 15:17:31 +0200

    first commit

---
+++ src/Regions.js
@@ -0,0 +1,92 @@
+export const Regions = [
+  {
+    id: 1,
+    name: "Shan state",
+    districts: [
+      {
+        id: 10,
+        name: "Aunglan",
+        townships: [
+          {
+            id: 100,
+            name: "Loilen",
+            lastInput: 123456,
+            formNumbers: 342456,
+            voterNumbers: 123456,
+            update: 342456,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Han state",
+    districts: [
+      {
+        id: 20,
+        name: "Old Aunglan",
+        townships: [
+          {
+            id: 200,
+            name: "Greenwich",
+            lastInput: 30865,
+            formNumbers: 22012,
+            voterNumbers: 30865,
+            update: 11002,
+          },
+        ],
+      },
+      {
+        id: 21,
+        name: "New Aunglan",
+        townships: [
+          {
+            id: 210,
+            name: "Nolita",
+            lastInput: 30865,
+            formNumbers: 88131,
+            voterNumbers: 30865,
+            update: 21392,
+          },
+        ],
+      },
+      {
+        id: 22,
+        name: "West Aunglan",
+        townships: [
+          {
+            id: 220,
+            name: "South Bay",
+            lastInput: 30865,
+            formNumbers: 10231,
+            voterNumbers: 30865,
+            update: 23001,
+          },
+        ],
+      },
+      {
+        id: 23,
+        name: "East Aunglan",
+        townships: [
+          {
+            id: 230,
+            name: "Bridgeport",
+            lastInput: 30865,
+            formNumbers: 33921,
+            voterNumbers: 30865,
+            update: 15400,
+          },
+          {
+            id: 231,
+            name: "Brockford",
+            lastInput: 30865,
+            formNumbers: 33921,
+            voterNumbers: 30865,
+            update: 15400,
+          },
+        ],
+      },
+    ],
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,17 +1,92 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { Regions } from "./Regions.js";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Test = () => {
+  const [showDistricts, setShowDistricts] = useState([]);
+  const [showTownships, setShowTownships] = useState([]);
+
+  const handleShowDistrictsButton = (index) => {
+    setShowDistricts([...showDistricts, ...index]);
+  };
+
+  const handleShowTownshipsButton = (index) => {
+    setShowTownships([...showTownships, ...index]);
+  };
+
+  return Regions.map((region) => {
+    return (
+      <div key={region.id}>
+        {region.name}
+        <button
+          key={region.id}
+          onClick={() =>
+            handleShowDistrictsButton(
+              region.districts.map((district) => district)
+            )
+          }
+        >
+          Show Districts
+        </button>
+        {region.districts.map((district) => {
+          return (
+            <>
+              {showDistricts[showDistricts.length - 1] ===
+              "undefined" ? null : showDistricts[showDistricts.length - 1] ===
+                region.districts.map((district) => district)[
+                  region.districts.map((district) => district).length - 1
+                ] ? (
+                <div
+                  key={district.id}
+                  style={
+                    showDistricts ? { display: "block" } : { display: "none" }
+                  }
+                >
+                  {district.name}
+                  <button
+                    key={district.id}
+                    onClick={() =>
+                      handleShowTownshipsButton(
+                        district.townships.map((township) => township)
+                      )
+                    }
+                  >
+                    Show Townships
+                  </button>
+                  {district.townships.map((township) => {
+                    return (
+                      <>
+                        {showTownships[showTownships.length - 1] ===
+                        "undefined" ? null : showTownships[
+                            showTownships.length - 1
+                          ] ===
+                          district.townships.map((township) => township)[
+                            district.townships.map((township) => township)
+                              .length - 1
+                          ] ? (
+                          <div
+                            key={township.id}
+                            style={
+                              showTownships
+                                ? { display: "block" }
+                                : { display: "none" }
+                            }
+                          >
+                            {township.name}
+                          </div>
+                        ) : null}
+                      </>
+                    );
+                  })}
+                </div>
+              ) : null}
+            </>
+          );
+        })}
+      </div>
+    );
+  });
+};
+
+ReactDOM.render(<Test />, document.getElementById("root"));
