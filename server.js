fb890d26c2355302f237d722129313d259e5d7f5
xiaoT <270199332@qq.com>
2020-07-27 11:21:30 +0800

    add: node server

--- server.js
+++ server.js
@@ -5,9 +5,23 @@
 
 const Koa = require('koa')
 const static = require('koa-static')
+const Router = require('koa-router')
 const app = new Koa()
 const port = process.env.PORT || 8080
+const router = new Router()
+
+// mock data
+const gallery = require('./__mocks__/galleryData.js')
+
+// static middleware
 app.use(static('./build'))
+// route config
+router.get('/api/gallery', (ctx) => {
+  ctx.body = gallery
+})
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
 
 app.listen(port)
 
