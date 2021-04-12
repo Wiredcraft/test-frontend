a85b3cbfce79e2f6929e8b6e029238abf9accb13
JT <wjtwangjianting@gmail.com>
2021-04-12 22:21:18 +0800

    Add client constant file

---
+++ client/src/const/load.js
@@ -0,0 +1,8 @@
+
+export const LoadStatus = {
+  Idle: 'Click to load more',
+  Loading: 'Loading...',
+  NoMoreData: 'Loading Completed',
+  Empty: 'Empty',
+  Failure: 'An error occurred, click to try again'
+};
