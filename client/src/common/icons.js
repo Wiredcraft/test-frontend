5819ec028f086097047c03c0433c294d6d4deacd
JT <wjtwangjianting@gmail.com>
2021-04-12 22:19:35 +0800

    Add client common icons

---
+++ client/src/common/icons.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FiSearch } from 'react-icons/fi';
+import { AiFillHome } from 'react-icons/ai';
+import { MdNotifications } from 'react-icons/md';
+import { FaUserCircle } from 'react-icons/fa';
+
+export const iconsMap = {
+  home: <AiFillHome />,
+  notification: <MdNotifications />,
+  user: <FaUserCircle />,
+  search: <FiSearch />,
+};
