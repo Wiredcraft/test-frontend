a9a417f3a3200aec194d35ee4d7988586f0cd384
JT <wjtwangjianting@gmail.com>
2021-04-12 22:17:51 +0800

    Add client index files

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import '@babel/polyfill'
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import './sass/index.scss';
+
+ReactDOM.render(<App />, document.getElementById('root'));
