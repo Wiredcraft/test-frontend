13eae15c37f090fa914efe1a794caaac12065887
Sum <sum@Sums-MacBook-Pro.local>
2021-04-14 22:33:17 +0800

    support scss

--- client/build/webpack.config.ts
+++ client/build/webpack.config.ts
@@ -14,10 +14,18 @@ module.exports = (env) => {
           use: 'ts-loader',
           exclude: /node_modules/,
         },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
+        },
       ],
     },
     resolve: {
-      extensions: ['.jsx', '.js', 'ts', 'tsx'],
+      extensions: ['.tsx', '.ts', '.js']
     },
     output: {
       path: path.resolve(__dirname, 'dist'),
