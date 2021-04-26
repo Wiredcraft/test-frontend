e195d3d02bb9b4235d799bac23de4f79c8e942be
gouflv <lv.gouf@gmail.com>
2021-04-26 20:15:22 +0800

    style(client): fixed page header

--- packages/client/src/components/PageHeader/PageHeader.tsx
+++ packages/client/src/components/PageHeader/PageHeader.tsx
@@ -1,12 +1,30 @@
 import {rgba} from 'polished'
+import {FC} from 'react'
 import styled from 'styled-components'
 
-const PageHeader = styled.header`
+const PageHeader: FC = (props) => {
+  return (
+    <PageHeaderWrapper>
+      <PageHeaderContent>{props.children}</PageHeaderContent>
+    </PageHeaderWrapper>
+  )
+}
+
+const PageHeaderWrapper = styled.div`
+  height: 70px;
+`
+
+const PageHeaderContent = styled.header`
+  position: fixed;
+  width: 100%;
+  top: 0;
+  z-index: 1001;
   box-sizing: border-box;
   display: flex;
   align-items: center;
   padding: 0 45px;
   height: 70px;
+  background-color: #fff;
   box-shadow: 0 4px 8px ${rgba('#000', 0.1)};
 `
 
