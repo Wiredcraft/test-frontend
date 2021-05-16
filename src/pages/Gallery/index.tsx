7c63c8f38a02aa50ae0a65a696c2e250f3e272b8
echooocc <echo2go@gmail.com>
2021-05-17 01:15:28 +0800

    add search no result tips

--- src/pages/Gallery/index.tsx
+++ src/pages/Gallery/index.tsx
@@ -11,8 +11,10 @@ const Gallery = () => {
   const [pictureList, setPictureList] = useState<pictureCard[]>([]);
   const [filteredPictureList, setFilteredPictureList] = useState<pictureCard[]>([]);
   const [searchQuery, setSearchQuery] = useState('');
+  const [showNoSearchTip, setShowNoSearchTip] = useState(false);
 
   const updateSearch = async (searchQuery: string) => {
+    setShowNoSearchTip(false);
     setFilteredPictureList(pictureList);
     setSearchQuery(searchQuery);
   }
@@ -21,11 +23,15 @@ const Gallery = () => {
     const filtered = pictureList.filter((item: pictureCard) => {
       return item.name === searchQuery;
     })
+    if(filtered.length == 0){
+      setShowNoSearchTip(true);
+    }
     setFilteredPictureList(filtered);
   }
  
   useEffect(() => {
     setIsLoading(true);
+    setShowNoSearchTip(false);
     const fetchData = async () => {
       const pictures = await getPictures();
       setPictureList(pictures.data.slice(0,10));
@@ -54,13 +60,25 @@ const Gallery = () => {
             >Search</button>
           </div>
         </div>
-        {isLoading ? 'loading' : ''}
-        <div className="wc-columns">
+        <div className="wc-columns mt-2">
           {filteredPictureList.map((item) => (
             <Card src={item.src} _id={item._id} name={item.name} key={item._id} />
           ))}
         </div>
+        {showNoSearchTip ? (
+          <div className="row mt-2">
+            <p>no search result</p>
+          </div>
+        ) : '' }
       </div>
+      {isLoading ? (
+          <div className="text-center m-5">
+            <div className="spinner-border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        )
+         : ''}
     </div>
   );
 };
