02436037d1a875be3f66dbf59266ead8d7f60bf5
Jin Xu <erlking@outlook.com>
2022-04-04 22:29:57 +0800

    style: lint warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import Masonry from "./components/Masonry/Masonry";
 import Navigation from "./components/Navigation/Navigation";
 import "./app.scss";
