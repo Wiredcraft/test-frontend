68714e2da6950f5851445f50eeda085c2db0f40f
Jin Xu <erlking@outlook.com>
2022-04-04 21:41:56 +0800

    test: rm meaningless test

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("Should render the app without error", () => {
-  const { container } = render(<App />);
-  expect(container).toBeTruthy();
-});
